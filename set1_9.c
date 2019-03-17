b87cf2ab4df6dce4cceb825dcdc28b73c874a569
sivasmart1303 <47888208+sivasmart1303@users.noreply.github.com>
2019-03-17 21:36:12 +0530

    Create set1_9.c

---
+++ set1_9.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+int main()
+{
+    int num=k;
+    long temp;
+    printf("enter the number");
+    scanf("%d",&num);
+    temp=num;
+    while(num>0)
+    {
+        printf("first k integer");
+    }
+    return 0;
+}
